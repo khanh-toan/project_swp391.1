1295b9141059e56ea73b18d471a1dc68b30ecc44
email <toandoitnow11@gmail.com>
2023-03-24 15:36:49 +0700

    Re-upload the project SWP

---
+++ src/java/Utills/Constance.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utills;
+
+/**
+ *
+ * @author DELL
+ */
+public class Constance {
+
+    /**
+     * SETUP kết nối database
+     */
+    final public static String PORTNUMBER = "1433";
+    final public static String SERVERNAME = "localhost";
+    final public static String DATABASENAME = "ChildrenCare";
+    final public static String USER_ACCOUNT_DATABASE = "sa";
+    final public static String USER_PASS_DATABASE = "sa123456";
+
+    /**
+     * setup gửi gmail
+     */
+    final public static String SENDGMAIL_TK = "docsanh630@gmail.com";
+    final public static String SENDGMAIL_MK = "nivgabqztcyyvoas";
+}
